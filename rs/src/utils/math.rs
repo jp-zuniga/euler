09759560a2af84a94f48ac1cae107a14f7d2fae5
jp-zuniga <jp.zuniga.dev@gmail.com>
2025-12-31 16:41:39 -0600

    utils(rs): iterative port of `nCr()`

--- rs/src/utils/math.rs
+++ rs/src/utils/math.rs
@@ -11,3 +11,26 @@ const fn gcd(mut a: u64, mut b: u64) -> u64 {
 pub const fn lcm(a: u64, b: u64) -> u64 {
     (a / gcd(a, b)) * b
 }
+
+#[allow(non_snake_case)]
+#[inline]
+pub fn nCr(n: u64, mut r: u64) -> u64 {
+    if r > n {
+        return 0;
+    }
+
+    if r > (n / 2) {
+        r = n - r;
+    }
+
+    r += 1;
+
+    let mut ways: u64 = 1;
+
+    for i in 1..r {
+        ways *= n - i + 1;
+        ways /= i;
+    }
+
+    ways
+}
